22d003ee90006a43c089321ed93d799623e19b07
nguyen <nguyenthcs430@gmail.com>
2024-11-19 19:54:58 +0700

    link mongo

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 async function connect() {
   try {
     await mongoose.connect(
+      // "mongodb://localhost:27017/book",
       process.env.DATABASE_URI ||
         "mongodb+srv://nguyenthcs430:hAxYIx8vF3gdaqmH@cluster0.sy5pjsk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
       {
